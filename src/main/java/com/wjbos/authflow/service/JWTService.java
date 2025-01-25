38d02be9cf6a695d143b1ec375e6ce7bdb7ee77c
WJbos <wjbosdev@gmail.com>
2025-01-25 10:24:12 +0200

    Finished JWT Service

--- src/main/java/com/wjbos/authflow/service/JWTService.java
+++ src/main/java/com/wjbos/authflow/service/JWTService.java
@@ -1,10 +1,17 @@
 package com.wjbos.authflow.service;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.security.Key;
+import java.time.Instant;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
@@ -26,11 +33,57 @@ public class JWTService {
         return claimsResolver.apply(claims);
     }
 
+    private Claims extractAlClaims(String token){
+        return Jwts
+                .parser()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
     public String generateToken(UserDetails userDetails) {
         return generateToken(new HashMap<>(),userDetails);
     }
 
     public String generateToken(Map<String, Object> claims, UserDetails userDetails) {
         return buildToken(claims,userDetails,jwtExpiration);
+    }
+
+    public Long  getExpiration(){
+        return jwtExpiration;
+    }
+
+    public String buildToken(Map<String,Object> extraClaims,
+                             UserDetails userDetails,
+                             long jwtExpiration
+    ){
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(Date.from(Instant.now()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(getSigningKey(), SignatureAlgorithm.ES256)
+                .compact();
+    }
+
+    public Boolean isTokenValid(String token, UserDetails userDetails) {
+        String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token){
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+
+    private Key getSigningKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
